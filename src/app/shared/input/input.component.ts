7777ee4fb6aeed95afbbb754bc679dbda2343d7e
Dante Marinho <dantecnia@hotmail.com>
2019-01-20 16:09:17 +0000

    # Aula 61 - Content Projection :: alteração do form simples para a criação de Content Projection, com a criação de uma pasta shared (para components de uso comum) e do component input e imports de ContentChild, interface AfterContentInit e NgModel

---
+++ src/app/shared/input/input.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input, ContentChild, AfterContentInit } from '@angular/core';
+import { NgModel } from '@angular/forms'
+
+@Component({
+  selector: 'mt-input-container',
+  templateUrl: './input.component.html'
+})
+export class InputComponent implements OnInit, AfterContentInit {
+
+  // labels (ex: Endereço, Número)
+  @Input() label: string
+  @Input() errorMessage: string
+
+  // objeto que vamos fazer referência
+  input: any
+
+  // como param do ContentChild podemos usar uma "diretiva" ou "ref a um elemento"
+  @ContentChild(NgModel) model: NgModel
+  // Precisa do momento certo para usar este model, temos que usar o método AfterContentInit
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // método que vai ser chamado exatamete quando o conteúdo que vai ficar no lugar do <ng-content> for definido
+  ngAfterContentInit(){
+    this.input = this.model
+
+    // vamos checar se o conteúdo que está sendo informado realmente existe uma tag ngModel
+    if(this.input === undefined){
+      throw new Error('Esse componente precisa ser usado com uma diretiva ngModel')
+    }
+  }
+
+  hasSuccess(): boolean{
+    return this.input.valid && (this.input.dirty || this.input.touched)
+  }
+
+  hasError(): boolean{
+    return this.input.invalid && (this.input.dirty || this.input.touched)
+  }
+
+}
